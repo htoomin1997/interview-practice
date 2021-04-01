c81ae8cd61a4a677c8770c282107f252487f7a07
Htoo <hm1487@nyu.edu>
2021-04-01 16:02:15 -0400

    refactored dataframe to be larger

--- TrueAccordInterview/question1.py
+++ TrueAccordInterview/question1.py
@@ -1,37 +1,71 @@
 import requests as reqs
 import json
 import pandas as pd
+import numpy as np
 
-def debtRequest():
-    debtRequest = reqs.get("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/debts")
-    debt_dict = json.loads(debtRequest.text)
-    return debt_dict
+def dataIntialization():
+    #On 4/1/2021 was taking a while to complete the response...
+    #payment plans request would sometimes time out!
+    try:
+        debtRequest = reqs.get("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/debts")
+        debt_dict = json.loads(debtRequest.text)
+        debts_df = pd.DataFrame.from_dict(debt_dict)
+        
+        print("request 1 completed")
 
-def paymentReq():
-    paymentRequest = reqs.get(" https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/payment_plans")
-    pay_dict = json.loads(paymentRequest.text)
-    return pay_dict
+        plansReq = reqs.get("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/payment_plans")
+        payment_plans = json.loads(plansReq.text)
+        plans_df = pd.DataFrame.from_dict(payment_plans)
 
-def dictionaryConcat(debts, payments):
-    my_dict = {}
-    for x in debts:
-        if (x['id'] in my_dict):
-            pass
+        print("request 2 completed")
+
+        paymentsReq = reqs.get("https://my-json-server.typicode.com/druska/trueaccord-mock-payments-api/payments")
+        payments = json.loads(paymentsReq.text)
+        payment_df = pd.DataFrame.from_dict(payments)
+
+        print("request 3 completed")
+
+        bigDf = pd.merge(left=debts_df, right=plans_df, left_on="id", right_on="debt_id", how="outer")
+        del bigDf['debt_id']
+
+        bigDf.rename(columns={"amount":"debt_amount", "id_x": "debt_id", "id_y": "payment_plan_id"}, inplace=True)
+        return (bigDf, payment_df)
+ 
+    except:
+        print("error?")
+
+def isEnrolled(concatDf):
+    isEnrolled = []
+    for x in concatDf['payment_plan_id']:
+        isEnrolled.append(False) if (np.isnan(x)) else isEnrolled.append(True)
+
+    concatDf["is_in_payment_plan"] = isEnrolled
+    return concatDf
+
+#So if they are enrolled in a payment plan, instead of using the payments, just use the payment plan amount?
+def remainingAmount(bigDf, payment_df):
+    payments_dict = {}
+    for index, row in payment_df.iterrows():
+        if (row['payment_plan_id'] in payments_dict):
+            payments_dict[row['payment_plan_id']] += row['amount']
         else:
-            my_dict[x['id']] = {"amount": x['amount'], "payments": []}
-    for x in payments:
-        if (x['id'] in my_dict):
-            my_dict[x['id']]['payments'].append(x)
-    print(my_dict)
+            payments_dict[row['payment_plan_id']] = row['amount']
+    print(payments_dict)
+
+    amounts = []
+    for index, row in bigDf.iterrows():
+        if (row['is_in_payment_plan'] == True):
+            amounts.append(row['amount_to_pay'] - payments_dict[row['payment_plan_id']])
+        else:
+            amounts.append(row['debt_amount'])
+    bigDf['remaining_amount'] = amounts
+    return bigDf
 
 
 def main():
-    debts = debtRequest()
-    for x in debts:
-        print(x)
-    payments = paymentReq()
-    for x in payments:
-        print(x)
-    dictionaryConcat(debts, payments)
+    concatDf, payments = dataIntialization()
+    newDf = isEnrolled(concatDf)
+    newDf = remainingAmount(newDf, payments)
+    print(newDf)
 
 main()
